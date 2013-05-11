0d848dd8314136c8aa39c4483bf855fe2f9b871e
Joe Noon <joenoon@gmail.com>
2013-05-11 15:45:25 -0700

    rmext_weak_attr_accessor uses new WeakRef in RM 2.0

--- lib/motion/accessors.rb
+++ lib/motion/accessors.rb
@@ -12,25 +12,10 @@ module RMExtensions
       def rmext_weak_attr_accessor(*attrs)
         attrs.each do |attr|
           define_method(attr) do
-            rmext_associatedValueForKey(attr.to_sym)
+            instance_variable_get("@#{attr}")
           end
           define_method("#{attr}=") do |val|
-            rmext_weaklyAssociateValue(val, withKey: attr.to_sym)
-            val
-          end
-        end
-      end
-
-      # creates an +attr_accessor+ like behavior, but the objects are stored with
-      # OBJC_ASSOCIATION_COPY.
-      # does not conform to KVO like attr_accessor does.
-      def rmext_copy_attr_accessor(*attrs)
-        attrs.each do |attr|
-          define_method(attr) do
-            rmext_associatedValueForKey(attr.to_sym)
-          end
-          define_method("#{attr}=") do |val|
-            rmext_atomicallyAssociateCopyOfValue(val, withKey: attr.to_sym)
+            instance_variable_set("@#{attr}", WeakRef.new(val))
             val
           end
         end
