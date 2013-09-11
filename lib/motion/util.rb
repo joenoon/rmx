220a3e3e5bc3bdc870698306d35c33f96b900a0b
Joe Noon <joenoon@gmail.com>
2013-09-10 21:33:01 -0700

    added Object#rmext_ivar, shortcut to instance_variable_get/set

--- lib/motion/util.rb
+++ lib/motion/util.rb
@@ -18,6 +18,19 @@ module RMExtensions
         raise "This method must be called on the main thread." unless NSThread.currentThread.isMainThread
       end
 
+      # Shortcut to instance_variable_get and instance_variable_get:
+      # 1 arg for instance_variable_get
+      # 2 args for instance_variable_set
+      def rmext_ivar(*args)
+        if args.size == 1
+          instance_variable_get("@#{args[0]}")
+        elsif args.size == 2
+          instance_variable_set("@#{args[0]}", args[1])
+        else
+          raise "rmext_ivar called with invalid arguments: #{args.inspect}"
+        end
+      end
+
     end
 
   end
