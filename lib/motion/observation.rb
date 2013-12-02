aa901ac09167d0a5010ec5b710ab8deca31894c2
Joe Noon <joenoon@gmail.com>
2013-12-02 11:23:48 -0800

    major refactoring of observations and events: - memory management was still not working correctly. - new method signatures, more verbose Class.new { } syntax required to create opaque block-like objects. - use native NSKeyValueObservingOptionInitial instead of calling the block immediately ourselves - observations fire inline if the main thread os handling them, otherwise they dispatch to the main queue - events always fire in the main dispatch queue (not 100% it will stay this way, or do the inline/dispatch pattern like observations) - observation callbacks now receive a ObservationResponse object as their only argument - event callbacks now receive an EventResponse object as their only argument - rmext_trigger now accepts a key and value, not N values - lots of blocks removed in favor of while x = y.pop style (dealloc-friendly syntax)

--- lib/motion/observation.rb
+++ lib/motion/observation.rb
@@ -8,26 +8,22 @@ module RMExtensions
         @rmext_observation_proxy ||= ObservationProxy.new(self)
       end
 
-      # observe an object.key. takes a block that will be called with the
-      # new value upon change.
-      #
-      # rmext_observe_passive(@model, "name") do |val|
-      #   p "name is #{val}"
-      # end
-      def rmext_observe_passive(object, key, &block)
-        rmext_observation_proxy.observe(object, key, &block)
+      def rmext_observe_passive(object, keyPath:key, withBlock:block)
+        rmext_observe(object, keyPath:key, options:(NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld), withBlock:block)
       end
 
-      # like +rmext_observe_passive+ but additionally fires the callback immediately.
-      def rmext_observe(object, key, &block)
-        rmext_observe_passive(object, key, &block)
-        block.call(object.send(key)) unless block.nil?
+      def rmext_observe(object, keyPath:key, withBlock:block)
+        rmext_observe(object, keyPath:key, options:(NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld | NSKeyValueObservingOptionInitial), withBlock:block)
+      end
+
+      def rmext_observe(object, keyPath:key, options:options, withBlock:block)
+        rmext_observation_proxy.observe(object, keyPath:key, options:options, withBlock:block)
       end
 
       # unobserve an existing observation
-      def rmext_unobserve(object, key)
+      def rmext_unobserve(object, keyPath:key)
         if @rmext_observation_proxy
-          @rmext_observation_proxy.unobserve(object, key)
+          @rmext_observation_proxy.unobserve(object, keyPath:key)
         end
       end
 
@@ -39,14 +35,14 @@ module RMExtensions
       end
 
       # register a callback when an event is trigger on this object
-      def rmext_on(event, &block)
-        rmext_observation_proxy.on(event, &block)
+      def rmext_on(event, inContext:context, withBlock:block)
+        rmext_observation_proxy.on(event, inContext:context, withBlock:block)
       end
 
       # remove a specific callback for an event on this object
-      def rmext_off(event, &block)
+      def rmext_off(event, inContext:context, withBlock:block)
         if @rmext_observation_proxy
-          @rmext_observation_proxy.off(event, &block)
+          @rmext_observation_proxy.off(event, inContext:context, withBlock:block)
         end
       end
 
@@ -57,10 +53,10 @@ module RMExtensions
         end
       end
 
-      # trigger an event with args on this object
-      def rmext_trigger(event, *args)
+      # trigger an event with value on this object
+      def rmext_trigger(event, value=nil)
         if @rmext_observation_proxy
-          @rmext_observation_proxy.trigger(event, *args)
+          @rmext_observation_proxy.trigger(event, value)
         end
       end
 
@@ -75,19 +71,26 @@ module RMExtensions
 
   end
 
+  class ObservationResponse
+    attr_accessor :context, :value, :old_value, :target, :key, :indexes, :kind
+  end
+
+  class EventResponse
+    attr_accessor :context, :value, :target, :event
+  end
+
   # # Proxy class used to hold the actual observation and watches for the real
   # # class intended to hold the observation to be deallocated, so the
   # # observation can be cleaned up.
   class ObservationProxy
-    COLLECTION_OPERATIONS = [ NSKeyValueChangeInsertion, NSKeyValueChangeRemoval, NSKeyValueChangeReplacement ]
-    DEFAULT_OPTIONS = NSKeyValueObservingOptionNew
 
     def initialize(obj)
+      @weak_object = WeakRef.new(obj)
       @desc = obj.inspect
-      @events = {}
+      @events = NSMapTable.weakToStrongObjectsMapTable
       @targets = {}
       if ::RMExtensions.debug?
-        p "created ObservationProxy for #{@desc}"
+        p "created ObservationProxy(#{@desc})"
       end
     end
 
@@ -95,26 +98,27 @@ module RMExtensions
       @did_dealloc = true
       cleanup
       if ::RMExtensions.debug?
-        p "dealloc ObservationProxy for #{@desc}"
+        p "dealloc ObservationProxy(#{@desc})"
       end
       super
     end
 
     def cleanup
       if ::RMExtensions.debug?
-        p "cleanup #{@desc}"
+        p "cleanup ObservationProxy(#{@desc})"
       end
       unobserve_all
       off_all
       true
     end
 
-    def observe(target, key_path, &block)
-      target.addObserver(self, forKeyPath:key_path, options:DEFAULT_OPTIONS, context:nil) unless registered?(target, key_path)
-      add_observer_block(target, key_path, &block)
+    def observe(target, keyPath:key_path, options:options, withBlock:block)
+      already_registered = registered?(target, key_path)
+      add_observer_block(target, key_path, block)
+      target.addObserver(self, forKeyPath:key_path, options:options, context:nil) unless already_registered
     end
 
-    def unobserve(target, key_path)
+    def unobserve(target, keyPath:key_path)
       return unless registered?(target, key_path)
       target.removeObserver(self, forKeyPath:key_path)
       remove_observer_block(target, key_path)
@@ -122,22 +126,20 @@ module RMExtensions
 
     def remove_observer_block(target, key_path)
       return if target.nil? || key_path.nil?
-
-      key_paths = @targets[target]
-      if !key_paths.nil? && key_paths.has_key?(key_path.to_s)
-        key_paths.removeObject(key_path.to_s)
+      key_path = key_path.to_s
+      target_hash = @targets[target]
+      if !target_hash.nil? && target_hash.has_key?(key_path)
+        target_hash.delete(key_path)
       end
     end
 
     def unobserve_all
-      keys = @targets.keys.clone
-      while keys.size > 0
-        target = keys.pop
+      keys = [] + @targets.keys
+      while target = keys.pop
         target_hash = @targets[target]
-        paths = target_hash.keys.clone
-        while paths.size > 0
-          key_path = paths.pop
-          target.removeObserver(self, forKeyPath:key_path)
+        paths = [] + target_hash.keys
+        while key_path = paths.pop
+          unobserve(target, keyPath:key_path)
         end
       end
       @targets.removeAllObjects
@@ -147,42 +149,80 @@ module RMExtensions
       !target.nil? && !@targets[target].nil? && @targets[target].has_key?(key_path.to_s)
     end
 
-    def add_observer_block(target, key_path, &block)
+    def add_observer_block(target, key_path, block)
       return if target.nil? || key_path.nil? || block.nil?
+      key_path = key_path.to_s
       @targets[target] ||= {}
-      @targets[target][key_path.to_s] ||= NSHashTable.weakObjectsHashTable
-      @targets[target][key_path.to_s].addObject block
+      @targets[target][key_path] ||= []
+      @targets[target][key_path].addObject block
     end
 
     # NSKeyValueObserving Protocol
 
     def observeValueForKeyPath(key_path, ofObject:target, change:change, context:context)
-      rmext_on_main_q do
+      m_desc = nil
+      if ::RMExtensions.debug?
+        m_desc = "~~> ObservationProxy(#{@desc})#observeValueForKeyPath(#{key_path}, ofObject:#{target.inspect.split(" ").first}>, ...)"
+        p "called", m_desc
+      end
+      action = proc do
         next if @did_dealloc
         next if target.nil?
-        key_paths = @targets[target] || {}
-        blocks = key_paths[key_path] || []
-        blocks.each do |block|
-          args = [ change[NSKeyValueChangeNewKey] ]
-          args << change[NSKeyValueChangeIndexesKey] if collection?(change)
-          block.call(*args)
+        key_paths = @targets[target]
+        next if key_paths.nil?
+        blocks = key_paths[key_path]
+        next if blocks.nil?
+        blocks = [] + blocks # get a new array that can be popped
+        if ::RMExtensions.debug?
+          p "blocks.size", blocks.size, m_desc
         end
+        while blk = blocks.pop
+          res = ObservationResponse.new
+          res.context = @weak_object
+          res.value = change[NSKeyValueChangeNewKey]
+          res.old_value = change[NSKeyValueChangeOldKey]
+          res.target = target
+          res.key = key_path
+          res.indexes = change[NSKeyValueChangeIndexesKey]
+          res.kind = change[NSKeyValueChangeKindKey]
+          blk.call(res)
+        end
+      end
+      if NSThread.currentThread.isMainThread
+        if ::RMExtensions.debug?
+          p "inline execution", m_desc
+        end
+        action.call
+      else
+        if ::RMExtensions.debug?
+          p "dispatch execution", m_desc
+        end
+        rmext_on_main_q(&action)
       end
     end
 
-    def collection?(change)
-      COLLECTION_OPERATIONS.include?(change[NSKeyValueChangeKindKey])
-    end
-
-    def on(event, &block)
+    def on(event, inContext:context, withBlock:block)
       return if event.nil? || block.nil?
-      @events[event.to_s] ||= []
-      @events[event.to_s].addObject block
+      event = event.to_s
+      context ||= self.class
+      unless context_events = @events.objectForKey(context)
+        context_events = {}
+        @events.setObject(context_events, forKey:context)
+      end
+      unless context_event_blocks = context_events.objectForKey(event)
+        context_event_blocks = []
+        context_events.setObject(context_event_blocks, forKey:event)
+      end
+      context_event_blocks.addObject block
     end
 
-    def off(event, &block)
-      return if event.nil? || block.nil? || !@events.key?(event.to_s)
-      @events[event.to_s].removeObject block
+    def off(event, inContext:context, withBlock:block)
+      return if event.nil? || block.nil?
+      event = event.to_s
+      context ||= self.class
+      return unless context_events = @events.objectForKey(context)
+      return unless context_event_blocks = context_events.objectForKey(event)
+      context_event_blocks.removeObject block
       nil
     end
 
@@ -190,10 +230,39 @@ module RMExtensions
       @events.removeAllObjects
     end
 
-    def trigger(event, *args)
-      return if event.nil? || !@events.key?(event.to_s)
-      @events[event.to_s].each do |block|
-        block.call(*args)
+    def trigger(event, value)
+      m_desc = nil
+      if ::RMExtensions.debug?
+        m_desc = "~~> ObservationProxy(#{@desc})#trigger(#{event}, #{value.inspect.split(" ").first }>)"
+        p "called", m_desc
+      end
+      rmext_on_main_q do
+        next if @did_dealloc
+        next if event.nil?
+        event = event.to_s
+        keyEnumerator = @events.keyEnumerator
+        contexts = []
+        while context = keyEnumerator.nextObject
+          contexts.push context
+        end
+        while context = contexts.pop
+          if context_events = @events.objectForKey(context)
+            if event_blocks = context_events[event]
+              blocks = [] + event_blocks
+              if ::RMExtensions.debug?
+                p "blocks.size", blocks.size, m_desc
+              end
+              while blk = blocks.pop
+                res = EventResponse.new
+                res.context = context
+                res.value = value
+                res.target = @weak_object
+                res.event = event
+                blk.call(res)
+              end
+            end
+          end
+        end
       end
       nil
     end
