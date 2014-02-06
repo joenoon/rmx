b54f7b3488d277cecedfb7c5c11638fe909d317e
Joe Noon <joenoon@gmail.com>
2014-02-06 10:10:22 -0800

    experiment with using rmext_zeroing_weak_attr_accessor as the default implementation. rmext_weak_attr_accessor is now an alias for rmext_zeroing_weak_attr_accessor (for now)

--- lib/motion/accessors.rb
+++ lib/motion/accessors.rb
@@ -4,20 +4,20 @@ module RMExtensions
 
     module Accessors
 
-      # creates an +attr_accessor+ like behavior, but the objects are
-      # stored with a WeakRef.
-      # does not conform to KVO like attr_accessor does.
-      def rmext_weak_attr_accessor(*attrs)
-        attrs.each do |attr|
-          define_method(attr) do
-            instance_variable_get("@#{attr}")
-          end
-          define_method("#{attr}=") do |val|
-            instance_variable_set("@#{attr}", WeakRef.new(val))
-            val
-          end
-        end
-      end
+      # # creates an +attr_accessor+ like behavior, but the objects are
+      # # stored with a WeakRef.
+      # # does not conform to KVO like attr_accessor does.
+      # def rmext_weak_attr_accessor(*attrs)
+      #   attrs.each do |attr|
+      #     define_method(attr) do
+      #       instance_variable_get("@#{attr}")
+      #     end
+      #     define_method("#{attr}=") do |val|
+      #       instance_variable_set("@#{attr}", val.nil? ? nil : WeakRef.new(val))
+      #       val
+      #     end
+      #   end
+      # end
 
       # creates an +attr_accessor+ like behavior, but the objects are
       # stored in an NSMapTable with strong keys (the attrs) and
@@ -38,6 +38,7 @@ module RMExtensions
           end
         end
       end
+      alias_method :rmext_weak_attr_accessor, :rmext_zeroing_weak_attr_accessor
 
     end
 
