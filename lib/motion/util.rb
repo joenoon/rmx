df4f9ae9850b0ba9dd394b00f91e5229ab777418
Joe Noon <joenoon@gmail.com>
2014-04-25 13:50:54 -0700

    remove rmext_retained, rmext_not_retained experiment, better to just create an intentional retain cycle with an ivar and clear it when you want the cycle to be broken, ie @hold_on = self. then later @hold_on = nil

--- lib/motion/util.rb
+++ lib/motion/util.rb
@@ -289,20 +289,6 @@ module RMExtensions
         nil
       end
 
-      def rmext_retained
-        unless @rmext_retained
-          @rmext_retained = true
-          retain
-        end
-      end
-
-      def rmext_not_retained
-        if @rmext_retained
-          @rmext_retained = false
-          release
-        end
-      end
-
     end
 
   end
