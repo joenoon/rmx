d898fae413dbe7509237093def85d7a07eb922fc
Joe Noon <joenoon@gmail.com>
2014-05-21 11:40:05 -0700

    added IdentityMap, synchronized strongtoweak hash

---
+++ lib/motion/identity_map.rb
@@ -0,0 +1,42 @@
+module RMExtensions
+
+  class IdentityMap < StrongToWeakHash
+
+    def initialize
+      super
+      @queue = Dispatch::Queue.new("#{NSBundle.mainBundle.bundleIdentifier}.rmx.IdentityMap.queue.#{object_id}")
+    end
+
+    def [](key)
+      @queue.sync do
+        return super
+      end
+    end
+
+    def []=(key, value)
+      @queue.sync do
+        return super
+      end
+    end
+
+    def delete(key)
+      @queue.sync do
+        return super
+      end
+    end
+
+    def keys
+      @queue.sync do
+        return super
+      end
+    end
+
+    def values
+      @queue.sync do
+        return super
+      end
+    end
+
+  end
+
+end
