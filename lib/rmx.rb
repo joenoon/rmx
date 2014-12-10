7d25244d040f2f77d4eaa1ffcb0bacb26fb548f9
Joe Noon <joenoon@gmail.com>
2014-12-10 15:11:10 -0800

    obfuscate RMX::Env

--- lib/rmx.rb
+++ lib/rmx.rb
@@ -48,8 +48,22 @@ Motion::Project::App.setup do |app|
   FileUtils.mkdir_p(File.expand_path("../build", File.dirname(__FILE__)))
   env_filename = File.expand_path("../build/env.rb", File.dirname(__FILE__))
   rmx_env = ENV['rmx_env'] == '1' ? ENV.to_hash : {}
+  rmx_env_string_parts = []
+  rmx_env_string_parts << "class RMX"
+  rmx_env_string_parts << "  rmx_env = {}"
+  # each env var is put into a hash with key and value obfuscated.
+  # a comment is prepended for the developer with the unobfuscated value, but does not make it into the binary.
+  # thanks to @lrz for the ord/chr idea.
+  rmx_env.each_pair do |k, v|
+    k_ords = k.to_s.scan(/./).map { |x| x.ord }
+    v_ords = v.to_s.scan(/./).map { |x| x.ord }
+    rmx_env_string_parts << "  # rmx_env[#{k.inspect}] = #{v.inspect}"
+    rmx_env_string_parts << "  rmx_env[#{k_ords.inspect}.map { |x| x.chr }.join] = #{v_ords.inspect}.map { |x| x.chr }.join"
+  end
+  rmx_env_string_parts << "  Env = rmx_env.freeze"
+  rmx_env_string_parts << "end"
   File.open(env_filename, "w") do |f|
-    f.puts %Q{class RMX\n  Env = #{rmx_env.inspect}\nend\n}
+    f.puts rmx_env_string_parts.join("\n")
   end
   App.log "RMX::Env", rmx_env.inspect
   app.files.unshift(env_filename)
