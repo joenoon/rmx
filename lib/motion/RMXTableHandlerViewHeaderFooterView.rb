d95ff9910dc5a30af998d1c95e9dfa3363c283a5
Joe Noon <joenoon@gmail.com>
2015-10-15 21:09:31 -0700

    either iOS or rubymotion seems to have an issue with an accessor named :tableView here, renaming

--- lib/motion/RMXTableHandlerViewHeaderFooterView.rb
+++ lib/motion/RMXTableHandlerViewHeaderFooterView.rb
@@ -1,12 +1,16 @@
 class RMXTableHandlerViewHeaderFooterView < RMXTableViewHeaderFooterView
 
-  RMX.new(self).weak_attr_accessor :tableView
+  RMX.new(self).weak_attr_accessor :rmx_tableView
   RMX.new(self).weak_attr_accessor :tableHandler
   attr_accessor :section
 
+  def tableView
+    self.rmx_tableView
+  end
+
   def context=(context)
     self.tableHandler = context[:tableHandler]
-    self.tableView = context[:tableView]
+    self.rmx_tableView = context[:tableView]
     self.section = context[:section]
     self.data = context[:data]
   end
